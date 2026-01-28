d129e63a9caa7d1cdc2ccf1ac4edd106941116de
Bakre Jaafar <bakrejafar126@gmail.com>
2026-01-28 19:08:40 +0100

    start

---
+++ www/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOeAOalt9rse5SAX1iwqIByEgqphSKI1E",
+  authDomain: "citizensreport-1905.firebaseapp.com",
+  projectId: "citizensreport-1905",
+  storageBucket: "citizensreport-1905.firebasestorage.app",
+  messagingSenderId: "624930538936",
+  appId: "1:624930538936:web:29967a5785403baa56bb4d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
